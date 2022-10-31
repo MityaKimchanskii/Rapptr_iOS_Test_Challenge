500ae914c968421811730c8c8e7338887217e0f0
Dmitrii Kim <mitya_kim@icloud.com>
2022-10-31 10:59:08 -0700

    Initial Commit

---
+++ Rapptr iOS Test Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rapptr iOS Test Challenge
+//
+//  Created by Mitya Kim on 10/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
